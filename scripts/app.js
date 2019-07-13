2bee00192e560fe6b0734fbd494f23a72e3b4f3d
Nina Daliva <ninadaliva@gmail.com>
2019-07-13 23:13:13 +0200

    Create section "About me" / "O mnie" + prepare simple structure for files + add bootstrap 4 + add two new webfonts

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Emi de Clam',
+        message: 'Welcome on my page!'
+    },
+    methods: {
+        // reverseMessage: function () {
+        //     this.message = this.message.split('').reverse().join('')
+        // }
+    }
+})
