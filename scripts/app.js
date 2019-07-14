64960188ad91cf9ced48d5cad6da118439d98cd0
Nina Daliva <ninadaliva@gmail.com>
2019-07-14 04:08:39 +0200

    Add gallery section. + install fancybox + add temporary header and footer

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,18 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        title: 'Emi de Clam',
-        message: 'Welcome on my page!'
-    },
+    el: "#app",
+        data: {
+            navigation: {
+                homePage: "Strona główna",
+                aboutMe: "Emi de Clam"
+            },
+            aboutMe: {
+                title: "O mnie",
+                header1: "Emilia „Emi de Clam” Marlewska",
+                header2: "Gdańsk, Polska",
+                smallText: "Grafik komputerowy, animatorka zajęć kreatywnych. <br />Absolwentka Polsko-Japońskiej Akademii Technik Komputerowych w Gdańsku.",
+                text: "Jestem autorką komiksów „Cwaniara” oraz „Goth & Pastel”.<br /> Od 2017 roku zajmuję się edycją graficzną komiksów.Na początku w wydawnictwie Yumegari, a od 2018 w wydawnictwie Hanami.<br /> Tytuły przy których pracowałam to między innymi: „Vampires Library”, „Siesta”, „Chłopaki z dwudziestego wieku”, „Czy chcesz zostać starą panną ?” czy też „Saga Winlandzka”.<br /> Prowadzę zajęcia kreatywne z komiksu dla młodzieży.Moje warsztaty odbyły się już między innymi w Bibliotece pod Żabką oraz w Bibliotece z Pasją w Gdyni.<br /> Jestem również liderem na zajęciach „Szkicownik” odbywających się w Domu Sąsiedzkim Pałacu w Gdańsku.<br /> "
+            }
+        },
     methods: {
         // reverseMessage: function () {
         //     this.message = this.message.split('').reverse().join('')
